021498a77d93f16b0ba9e5c10191d8da2825451d
youtao <lytlytlc@outlook.com>
2016-06-27 00:27:28 +0800

    添加SignalR

---
+++ MVC-Learn/SignalR/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MVC_Learn.SignalR
+{
+    public class ChatHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public override Task OnConnected()
+        {
+            var connectionId = this.Context.ConnectionId;
+            return base.OnConnected();
+        }
+
+
+
+        public override Task OnReconnected()
+        {
+            var connectionId = this.Context.ConnectionId;
+            return base.OnReconnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var connectionId = this.Context.ConnectionId;
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
--- MVC-Learn/Startup.cs
+++ MVC-Learn/Startup.cs
@@ -1,15 +1,21 @@
-﻿using Owin;
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+[assembly: OwinStartup(typeof(MVC_Learn.Startup))]
 namespace MVC_Learn
 {
+
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
+            //var sqlConnectionString = @"Server=.Database=SignalR;Integrated Security=True;";
+            //GlobalHost.DependencyResolver.UseSqlServer(sqlConnectionString);
             app.MapSignalR();
         }
     }
