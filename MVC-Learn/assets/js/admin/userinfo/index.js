9f3694c58241a7b9afe64fbdb9585098935d93bf
youtao <lytlytlc@gmail.com>
2016-07-14 23:12:58 +0800

    完善显示效果

--- MVC-Learn/assets/js/admin/userinfo/index.js
+++ MVC-Learn/assets/js/admin/userinfo/index.js
@@ -1,90 +1,116 @@
 ﻿$(function () {
-    //$('#main-layout').layout();
-    //$('#main-layout').layout('add',
-    //{
-    //    region: 'north',
-    //    height: 100        
-    //});
-    //$('#main-layout').layout('add',
-    //{
-    //    region: 'south',
-    //    height: 200
-    //});
-    //$('#main-layout').layout('add',
-    //{
-    //    region: 'center'
-    //});
-
-    //$('#main-layout').layout('add',
-    //{
-    //    region: 'east',
-    //    height: 100,
-    //    width:300
-    //});
-
-    $('#user-list')
-        .datagrid({
-            url: '',
-            method: 'GET',
-            striped: true,
-            loadMsg: '拼命加载中...',
-            rownumbers: true,
-            pagination: true,
-            singleSelect:true,
-            checkOnSelect:false,
-            selectOnCheck: false,            
-            columns: [
-                [
-                    { field: 'Unique', checkbox: true },
-                    { field: 'UserName', title: '用户名', width: '20%', align: 'center' },
-                    { field: 'NickName', title: '昵称', width: '20%', align: 'center' },
-                    { field: 'CreateTime', title: '注册日期', width: '20%', align: 'center' },
-                    { field: 'Age', title: '年龄', width: '20%', align: 'center' },
-                    {
-                        field: 'Online', title: '是否在线', width: '10%', align: 'center', styler: function (value, row, index) {
-                            var color = 'red';
-                            if (value==='是') {
-                                color = 'green';
-                            }
-                            return 'background-color:'+color;
-                        }
+    $('#user-list').datagrid({
+        url: '',
+        method: 'GET',
+        striped: true,
+        loadMsg: '拼命加载中...',
+        rownumbers: true,
+        pagination: true,
+        singleSelect: true,
+        checkOnSelect: false,
+        selectOnCheck: false,
+        columns: [
+            [
+                { field: 'Unique', checkbox: true },
+                {
+                    field: 'UserName', title: '用户名', width: '20%', align: 'center', formatter: function (value, row, index) {
+                        return `${value}-${row.Unique}`;
                     }
-                ]
-            ],
-            data: [
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
-                { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+                },
+                { field: 'NickName', title: '昵称', width: '20%', align: 'center' },
+                { field: 'CreateTime', title: '注册日期', width: '20%', align: 'center' },
+                { field: 'Age', title: '年龄', width: '20%', align: 'center' },
+                {
+                    field: 'Online', title: '是否在线', width: '10%', align: 'center', styler: function (value, row, index) {
+                        var color = 'red';
+                        if (value === '是') {
+                            color = 'green';
+                        }
+                        return 'opacity:0.7;background-color:' + color;
+                    }
+                }
             ]
+        ],
+
+        data: [
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '否' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+            { Unique: '123456', UserName: 'youtao', NickName: 'youtao', CreateTime: '2016-07-14', Age: '18', Online: '是' },
+        ],
+        //toolbar: [{
+        //    iconCls: 'icon-add',
+        //    text: '添加用户',
+        //    handler: function () {
+        //        $.messager.show({
+        //            title: '添加用户',
+        //            msg: '此功能稍后开放',
+        //            timeout: 5000,
+        //            showType: 'slide'
+        //        });
+        //    }
+        //}, '-', {
+        //    text: '删除用户',
+        //    iconCls: 'icon-remove',
+        //    handler: function () {
+        //        var list = [];
+        //        $('#user-list').datagrid('getChecked').forEach(function (item) {
+        //            list.push(item.Unique);
+        //        });
+        //    }
+        //}, '-', {
+        //    text: '工具',
+        //    iconCls: 'icon-more',
+        //    handler: function () {
+
+        //    }
+        //}]
+        toolbar: '#user-list-toolbar'
+    });
+
+    $('#add-user').click(function () {
+        $.messager.show({
+            title: '添加用户',
+            msg: '此功能稍后开放',
+            timeout: 5000,
+            showType: 'slide'
         });
+    });
+    $('#delete-user').click(function () {
+        var list = [];
+        $('#user-list').datagrid('getChecked').forEach(function (item) {
+            list.push(item.Unique);
+        });
+        console.log(list);
+    });
+
 
 });
