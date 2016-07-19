4801d4efb53f40036e83c1fc5b60b95fe650a548
youtao <lytlytlc@gmail.com>
2016-07-19 23:12:46 +0800

    vs生成menu控制器

--- MVC-Learn/Areas/Admin/Controllers/MenuController.cs
+++ MVC-Learn/Areas/Admin/Controllers/MenuController.cs
@@ -11,92 +11,112 @@ using MVC_Learn.Models;
 
 namespace MVC_Learn.Areas.Admin.Controllers
 {
-    public class UserInfoController : Controller
+    public class MenuController : Controller
     {
         private LearnDbContext db = new LearnDbContext();
 
-        // GET: Admin/UserInfo
-        public ActionResult Index()
+        // GET: Admin/Menu
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var menu = db.Menu.Include(m => m.Parent);
+            return View(await menu.ToListAsync());
         }
 
-        // GET: Admin/UserInfo/Details/5
+        // GET: Admin/Menu/Details/5
         public async Task<ActionResult> Details(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserInfo userInfo = await db.UserInfo.FindAsync(id);
-            if (userInfo == null)
+            Menu menu = await db.Menu.FindAsync(id);
+            if (menu == null)
             {
                 return HttpNotFound();
             }
-            return View(userInfo);
+            return View(menu);
         }
 
-        // GET: Admin/UserInfo/Create
+        // GET: Admin/Menu/Create
         public ActionResult Create()
         {
+            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title");
             return View();
         }
 
-        // POST: Admin/UserInfo/Create
+        // POST: Admin/Menu/Create
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,UserName,Password,NickName,LoginTime,SignoutTime,CreateTime")] UserInfo userInfo)
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title,Url,Icon,ParentId,CreateTime")] Menu menu)
         {
             if (ModelState.IsValid)
             {
-                db.UserInfo.Add(userInfo);
+                db.Menu.Add(menu);
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
-            return View(userInfo);
+            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
+            return View(menu);
         }
 
-        // GET: Admin/UserInfo/Edit/5
+        // GET: Admin/Menu/Edit/5
         public async Task<ActionResult> Edit(long? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserInfo userInfo = await db.UserInfo.FindAsync(id);
-            if (userInfo == null)
+            Menu menu = await db.Menu.FindAsync(id);
+            if (menu == null)
             {
                 return HttpNotFound();
             }
-            return View(userInfo);
+            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
+            return View(menu);
         }
 
-        // POST: Admin/UserInfo/Edit/5
+        // POST: Admin/Menu/Edit/5
         // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
         // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,UserName,Password,NickName,LoginTime,SignoutTime,CreateTime")] UserInfo userInfo)
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Url,Icon,ParentId,CreateTime")] Menu menu)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(userInfo).State = EntityState.Modified;
+                db.Entry(menu).State = EntityState.Modified;
                 await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(userInfo);
+            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
+            return View(menu);
         }
 
-        // POST: Admin/UserInfo/Delete/5
+        // GET: Admin/Menu/Delete/5
+        public async Task<ActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Menu menu = await db.Menu.FindAsync(id);
+            if (menu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Admin/Menu/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> DeleteConfirmed(long id)
         {
-            UserInfo userInfo = await db.UserInfo.FindAsync(id);
-            db.UserInfo.Remove(userInfo);
+            Menu menu = await db.Menu.FindAsync(id);
+            db.Menu.Remove(menu);
             await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
