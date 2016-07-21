801049d9802b8f51d8925b7cfbcf9bd1a2edee7b
youtao <lytlytlc@gmail.com>
2016-07-21 23:02:43 +0800

    menu 前端:更改 后端:更删改查

--- MVC-Learn/Areas/Admin/Controllers/MenuController.cs
+++ MVC-Learn/Areas/Admin/Controllers/MenuController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC_Learn.Models;
+using EntityFramework.Extensions;
 
 namespace MVC_Learn.Areas.Admin.Controllers
 {
@@ -32,109 +33,54 @@ namespace MVC_Learn.Areas.Admin.Controllers
                 e.Id,
                 e.Title,
                 e.Url,
-                e.Icon,                
+                e.Icon,
                 HasChildren = e.Children.Any()
             }).ToListAsync();
             return this.Json(json, JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Admin/Menu/Details/5
-        public async Task<ActionResult> Details(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Menu menu = await db.Menu.FindAsync(id);
-            if (menu == null)
-            {
-                return HttpNotFound();
-            }
-            return View(menu);
-        }
-
-        // GET: Admin/Menu/Create
-        public ActionResult Create()
-        {
-            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title");
-            return View();
-        }
-
-        // POST: Admin/Menu/Create
-        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
-        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "Id,Title,Url,Icon,ParentId,CreateTime")] Menu menu)
+        //[ValidateAntiForgeryToken]
+        public JsonResult Create([Bind(Include = "Title,Url,Icon,ParentId")] Menu menu)
         {
+            var menuId = -1L;
             if (ModelState.IsValid)
             {
                 db.Menu.Add(menu);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                db.SaveChanges();
+                menuId = menu.Id;
             }
-
-            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
-            return View(menu);
+            return Json(new { MenuId = menuId });
         }
 
-        // GET: Admin/Menu/Edit/5
-        public async Task<ActionResult> Edit(long? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Menu menu = await db.Menu.FindAsync(id);
-            if (menu == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
-            return View(menu);
-        }
-
-        // POST: Admin/Menu/Edit/5
-        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
-        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Url,Icon,ParentId,CreateTime")] Menu menu)
+        //[ValidateAntiForgeryToken]
+        public async Task<JsonResult> Edit([Bind(Include = "Id,Title,Url,Icon")] Menu param)
         {
+            var result = false;
             if (ModelState.IsValid)
             {
-                db.Entry(menu).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+                result = true;
+                await db.Menu.Where(e => e.Id == param.Id).UpdateAsync(e => new Menu()
+                {
+                    Title = param.Title,
+                    Url = param.Url,
+                    Icon = param.Icon
+                });
             }
-            ViewBag.ParentId = new SelectList(db.Menu, "Id", "Title", menu.ParentId);
-            return View(menu);
+            return this.Json(result);
         }
 
-        // GET: Admin/Menu/Delete/5
-        public async Task<ActionResult> Delete(long? id)
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<JsonResult> Delete(long id)
         {
-            if (id == null)
+            var result = false;
+            result = await db.Menu.Where(e => e.Id == id).UpdateAsync(e => new Menu()
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Menu menu = await db.Menu.FindAsync(id);
-            if (menu == null)
-            {
-                return HttpNotFound();
-            }
-            return View(menu);
-        }
 
-        // POST: Admin/Menu/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(long id)
-        {
-            Menu menu = await db.Menu.FindAsync(id);
-            db.Menu.Remove(menu);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            }) > 0; // TODO:软删除
+            return Json("");
         }
 
         protected override void Dispose(bool disposing)
--- MVC-Learn/assets/js/admin/menu/index.js
+++ MVC-Learn/assets/js/admin/menu/index.js
@@ -1,36 +1,93 @@
 ﻿$(function () {
+    var config = {
+        editingId: undefined
+    }
+
     $('#menu-list').treegrid({
         url: '/admin/menu/getmenu',
         idField: 'Id',
         treeField: 'Title',
-        method: 'GET',        
+        method: 'GET',
         loadMsg: '拼命加载中...',
         rownumbers: true,
         animate: true,
-        lines:true,
+        lines: true,
         columns: [[
             {
-                field: 'Title', title: 'title', width: '30%'
+                field: 'Title', title: 'title', width: '30%', editor: 'text'
             },
-            { field: 'Url', title: 'url', width: '40%' },
+            { field: 'Url', title: 'url', width: '40%', editor: 'text' },
             {
-                field: 'Icon', title: 'icon', width: '20%', align:'center', formatter: function (node) {
+                field: 'Icon', title: 'icon', width: '20%', align: 'center', editor: 'text', formatter: function (node) {
                     var html = '<i class="' + node + '"></i>&nbsp;&nbsp;&nbsp;' + node;
                     return html;
                 }
             }
         ]],
-        loadFilter: function (data, parent) {            
-            for (var i = 0; i < data.length; i++) {                
+        toolbar: [
+            {
+                text: '添加',
+                iconCls: 'fa fa-create',
+                handler: function () {
+
+                }
+            }, '-', {
+                text: '编辑',
+                iconCls: 'fa fa-edit',
+                handler: function () {
+                    if (config.editingId != undefined) {
+                        $('#menu-list').treegrid('select', config.editingId);
+                        return;
+                    }
+                    var row = $('#menu-list').treegrid('getSelected');
+                    if (row) {
+                        config.editingId = row.Id
+                        $('#menu-list').treegrid('beginEdit', config.editingId);
+                    }
+                }
+            }, '-', {
+                text: '保存',
+                iconCls: 'fa fa-save',
+                handler: function () {
+                    if (config.editingId != undefined) {
+                        var t = $('#tg');
+                        $('#menu-list').treegrid('endEdit', config.editingId);
+                        config.editingId = undefined;
+                    }
+                }
+            }, '-', {
+                text: '取消',
+                iconCls: 'fa fa-user',
+                handler: function () {
+                    if (config.editingId != undefined) {
+                        $('#menu-list').treegrid('cancelEdit', config.editingId);
+                        config.editingId = undefined;
+                    }
+
+
+                }
+            }],
+        loadFilter: function (data, parent) {
+            for (var i = 0; i < data.length; i++) {
                 if (data[i].HasChildren) {
                     data[i].state = 'closed';
                     data[i].children = [];
                 }
-            }            
+            }
             return data;
         },
         onBeforeExpand: function (node) {
             $('#menu-list').treegrid('options').url = "/admin/menu/getmenu?ParentId=" + node.Id;
+        },
+        onAfterEdit: function (node) { // TODO:判断是否更新或添加成功
+            $.post('/admin/menu/edit', {
+                Id: node.Id,
+                Title: node.Title,
+                Url: node.Url,
+                Icon: node.Icon,
+            }, function (response) {
+                console.log(response);
+            });
         }
     });
 });
