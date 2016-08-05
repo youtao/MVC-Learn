d5a154ab4c80489e987e3c2441c55ad046df4c33
youtao <lytlytlc@gmail.com>
2016-08-06 02:01:27 +0800

    ConsoleApplication

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LearnDbContext db = new LearnDbContext())
+            {
+                db.UserInfo.Select(e => new
+                {
+                    Length = e.NickName.Length
+                }).ToList();
+            }
+        }
+    }
+}
