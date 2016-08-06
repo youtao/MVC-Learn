cc45811053068a6cccfbf9bfb8a5ede174f1f2c8
youtao <lytlytlc@gmail.com>
2016-08-07 01:45:02 +0800

    ef sql

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +14,33 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            Database.SetInitializer<LearnDbContext>(null);
             using (LearnDbContext db = new LearnDbContext())
             {
-                db.UserInfo.Select(e => new
-                {
-                    Length = e.NickName.Length
-                }).ToList();
+                db.UserInfo.ToList();                
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var sql = @"select Id,Title,Url,Icon,MenuOrder,ParentId,CreateTime,[Delete] from dbo.System_Menu";
+                var query = db.Database.SqlQuery<Menu>(sql);
+                var bySql = query.ToList();
+                stopwatch.Stop();
+                Console.WriteLine(stopwatch.ElapsedMilliseconds);                
+
+                stopwatch.Restart();
+                var byEf = db.Menu.ToList();
+                stopwatch.Stop();
+                Console.WriteLine(stopwatch.ElapsedMilliseconds);
+                Console.ReadKey();
             }
         }
+    }
+
+    public class Menu
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
     }
 }
