9f8ebe638fafdb7f3a9d9b39931dd1095ea7fd3d
youtao <lytlytlc@gmail.com>
2016-09-08 23:45:56 +0800

    修改框架版本

---
+++ WebUI.Test/Areas/Admin/Controllers/HomeControllerFacts.cs
@@ -0,0 +1,14 @@
+﻿using WebUI.Areas.Admin.Controllers;
+
+namespace WebUI.Test.Areas.Admin.Controllers
+{
+    public class HomeControllerFacts
+    {
+        private HomeController _controller = null;
+
+        public HomeControllerFacts()
+        {
+            this._controller = new HomeController();
+        }
+    }
+}
---
+++ WebUI.Test/RouteConfigFacts.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Xunit;
+
+namespace WebUI.Test
+{
+    public class RouteConfigFacts
+    {
+        [Fact]
+        public void RegisterRoutes_Valid()
+        {
+            var mock = new Mock<HttpContextBase>();
+            mock.Setup(e => e.Request.AppRelativeCurrentExecutionFilePath).Returns("~/handler.axd");
+            var routes = new RouteCollection();
+            WebUI.RouteConfig.RegisterRoutes(routes);
+
+            RouteData routeData = routes.GetRouteData(mock.Object);
+
+            Assert.NotNull(routeData);
+            Assert.IsAssignableFrom<StopRoutingHandler>(routeData.RouteHandler);
+
+        }
+
+        [Fact]
+        public void RegisterRoutes_Valid_ToHomePage()
+        {
+            var mock = new Mock<HttpContextBase>();
+            mock.Setup(e => e.Request.AppRelativeCurrentExecutionFilePath).Returns("~/Admin/Menu");
+            var routes = new RouteCollection();
+            WebUI.RouteConfig.RegisterRoutes(routes);
+
+            RouteData routeData = routes.GetRouteData(mock.Object);
+
+            Assert.NotNull(routeData);
+            Assert.Equal("Admin", routeData.Values["controller"]);
+            Assert.Equal("Menu", routeData.Values["action"]);
+            Assert.Equal(UrlParameter.Optional, routeData.Values["id"]);
+        }
+    }
+}
