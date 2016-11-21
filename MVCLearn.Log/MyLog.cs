1cfa89225d9696afd01ea8c44e3c7c261e261401
youtao <lytlytlc@gmail.com>
2016-11-21 14:42:49 +0800

    Log

---
+++ MVCLearn.Log/MyLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NLog;
+
+namespace MVCLearn.Log
+{
+    public static class MyLog
+    {
+        private static readonly Logger ErrorLogger = LogManager.GetLogger("Error_Logger");
+
+        public static void Error(Exception exception)
+        {
+            LogEventInfo eventInfo = LogEventInfo.Create(LogLevel.Error, string.Empty, string.Empty);
+            eventInfo.Properties["Messages"] = exception.ToString();
+            ErrorLogger.Log(eventInfo);
+        }
+    }
+}
