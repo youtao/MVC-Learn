baf4f08e7836bcf8ad9f0f6279bff89dec97c7f9
youtao <lytlytlc@gmail.com>
2016-11-21 14:30:58 +0800

    Config

---
+++ MVCLearn.Config/AutoMapperConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+
+namespace MVCLearn.Config
+{
+    public class AutoMapperConfig
+    {
+        public static void MapperInitialize()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile<DefaultProfile>();
+            });
+        }
+    }
+
+    public class DefaultProfile : Profile
+    {
+        public DefaultProfile()
+        {
+
+        }
+    }
+}
---
+++ MVCLearn.Config/EntityFramewokConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure;
+using MVCLearn.ModelDbContext;
+
+namespace MVCLearn.Config
+{
+    public class EntityFramewokConfig
+    {
+        /// <summary>
+        /// EntityFramewok热加载
+        /// </summary>
+        public static void HeatLoad()
+        {
+#if !DEBUG
+            Database.SetInitializer<CoalCostDbContext>(null);
+#endif
+            using (var dbContext = new LearnDbContext())
+            {
+                var objectContext = ((IObjectContextAdapter) dbContext).ObjectContext;
+                var mappingCollection =
+                    (StorageMappingItemCollection) objectContext.MetadataWorkspace.GetItemCollection(DataSpace.CSSpace);
+                mappingCollection.GenerateViews(new List<EdmSchemaError>());
+            }
+        }
+    }
+}
