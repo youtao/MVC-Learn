ad6d0c84604805ac0b5adb88d19192051ff60a42
youtao <lytlytlc@gmail.com>
2016-11-21 16:32:37 +0800

    Service Test

---
+++ MVCLearn.Service.Test/BaseFact.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Web;
+using Moq;
+using MVCLearn.Config;
+
+namespace MVCLearn.Service.Test
+{
+    public class BaseFact
+    {
+        public readonly HttpContextBase HttpContext;
+
+        public BaseFact()
+        {
+            AutoMapperConfig.MapperInitialize();
+            var mockContext = new Mock<HttpContextBase>();
+            mockContext.Setup(e => e.Items).Returns(new Hashtable());
+            HttpContext = mockContext.Object;
+        }
+    }
+}
