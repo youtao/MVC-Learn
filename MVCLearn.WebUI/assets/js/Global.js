c8f64c5731d707546a8b087d335579c78d151229
youtao <lytlytlc@gmail.com>
2016-11-23 02:03:09 +0800

    主题

--- MVCLearn.WebUI/assets/js/Global.js
+++ MVCLearn.WebUI/assets/js/Global.js
@@ -1,3 +1,49 @@
-﻿var GlobalConfig = {
+﻿$(function () {
+    letfMenu();
+    $('#page-body').resize(function (e) {
+        footer();
+    });
+});
+var GlobalConfig = {
     Server: 'http://localhost:33826/api/'
 };
+function letfMenu() {
+    $.get('/assets/json/menus.json', null, function (res) {
+        var html = recursiveMenu(res);
+        $('#left-menu-inner-heard').after(html);
+        $('#left-menu-inner').perfectScrollbar();
+    });
+}
+function recursiveMenu(children) {
+    var ul = '<ul>';
+    for (var i = 0; i < children.length; i++) {
+        var item = children[i];
+        var li = '<li>';
+        li += '<a href="javascript:void(0)"; data-url="' + item.url + '">' + item.title + '</a>';
+        if (item.children.length > 0) {
+            li += arguments.callee(item.children);
+        }
+        li += '</li>';
+        ul += li;
+    }
+    ul += '</ul>';
+    return ul;
+}
+
+function footer() {
+    var offsetHeight = document.body.offsetHeight;
+    var $pageHeard = $('#page-heard');
+    var $pageTitle = $('#page-title');
+    var $pageBody = $('#page-body');
+    var $pageFooter = $('#page-footer');
+    var marginTop = offsetHeight -
+        $pageHeard.outerHeight() -
+        $pageTitle.outerHeight() -
+        $pageBody.outerHeight() -
+        $pageFooter.outerHeight() - 30;
+    if (marginTop > 0) {
+        $pageFooter.css('margin-top', marginTop);
+    } else {
+        $pageFooter.css('margin-top', 30);
+    }
+}
--- MVCLearn.WebUI/assets/js/admin/User/Index.js
+++ MVCLearn.WebUI/assets/js/admin/User/Index.js
@@ -19,34 +19,5 @@
     }
 };
 $(function () {
-
-    loadLeftMenu();
     currentpage.init();
 });
-
-function loadLeftMenu() {
-    $.get('/assets/json/menus.json', null, function (res) {
-        builderMenu(res);
-    });
-}
-
-function builderMenu(menus) {
-    var html = recursiveMenu(menus);
-    $('#left-menu').html(html);
-}
-
-function recursiveMenu(children) {
-    var ul = '<ul>';
-    for (var i = 0; i < children.length; i++) {
-        var item = children[i];
-        var li = '<li>';
-        li += '<a href="javascript:void(0)"; data-url="' + item.url + '">' + item.title + '</a>';
-        if (item.children.length > 0) {
-            li += arguments.callee(item.children);
-        }
-        li += '</li>';
-        ul += li;
-    }
-    ul += '</ul>';
-    return ul;
-}
