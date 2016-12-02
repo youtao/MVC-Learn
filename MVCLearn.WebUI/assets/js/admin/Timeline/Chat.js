bae138eff7920fc259a315030b09678528d84ac8
youtao <lytlytlc@gmail.com>
2016-12-02 18:41:39 +0800

    聊天界面

--- MVCLearn.WebUI/assets/js/admin/Timeline/Chat.js
+++ MVCLearn.WebUI/assets/js/admin/Timeline/Chat.js
@@ -1,7 +1,11 @@
 ﻿$(function () {
+
+    var lastHeight = $('#chat-input').outerHeight();
+
+
     onresize();
     $(window).resize(onresize);
-    $('#input-area').resize(onresize);
+    $('#chat-input').resize(onresize);
     var vue = new Vue({
         el: '#chat-app',
         data: {
@@ -10,12 +14,6 @@
     });
     $.connection.hub.url = 'http://localhost:26949/signalr';
     var chathub = $.connection.chatHub;
-    chathub.client.start = function (connectionid) {
-        console.log('连接:' + connectionid);
-    };
-    chathub.client.leave = function (connectionid) {
-        console.log('离开:' + connectionid);
-    };
     chathub.client.sendMessages = function (connectionid, messages) {
         vue.messages.push({
             aligned: 'left-aligned',
@@ -26,9 +24,7 @@
             content: messages
         });
     };
-    $.connection.hub.start().done(function () {
-
-    });
+    $.connection.hub.start();
     $('#send-messages').click(function () {
         var messages = $('#chat-input').text();
         $('#chat-input').text('');
@@ -41,28 +37,41 @@
         });
         chathub.server.sendMessages(messages);
         $('#send-messages').attr('disabled', 'disabled');
+        lastHeight = $(this).outerHeight();
     });
 
     $('#chat-conversation').perfectScrollbar();
     $('div.chat-input').perfectScrollbar();
 
     $('#chat-input').bind('input propertychange', function () {
+        if ($(this).outerHeight() !== lastHeight) {
+            $('#chat-conversation').perfectScrollbar('update');
+            $('#chat-conversation').scrollTop($('#chat-conversation')[0].scrollHeight);
+            onresize();
+        }
+        lastHeight = $(this).outerHeight();
         var text = $(this).text();
-        if (text.trim().length>0) {
+        if (text.trim().length > 0) {
             $('#send-messages').removeAttr('disabled');
         } else {
             $('#send-messages').attr('disabled', 'disabled')
         }
     });
+
+    vue.$watch('messages', function () {
+        $('#chat-conversation').perfectScrollbar('update');
+        $('#chat-conversation').scrollTop($('#chat-conversation')[0].scrollHeight);
+        onresize();
+    });
 });
 
-
-function onresize(e) {
-    var height = $(window).height();
+function onresize() {
+    var $window = $(window);
     var $inputArea = $('#input-area');
     var $chatConversation = $('#chat-conversation');
-    var maxHeight = height -
-        $inputArea.outerHeight();
-    $chatConversation.css('height', maxHeight);
-    $('#chat-conversation').scrollTop($('#chat-conversation')[0].scrollHeight);
+
+    var height = $window.height() - $inputArea.outerHeight();
+    $chatConversation.css('height', height)
+        .scrollTop($('#chat-conversation')[0].scrollHeight);
+    $inputArea.show();
 }
