18e1ba32fac5298cec06708399c497da48e3643b
youtao <lytlytlc@gmail.com>
2016-12-15 23:48:35 +0800

    _app.layout 全局点击事件

--- src/MVCLearn.WebUI/assets/js/_app.layout.js
+++ src/MVCLearn.WebUI/assets/js/_app.layout.js
@@ -1,3 +1,19 @@
 ﻿$(function () {
-
+    _layout.init();
 });
+
+var _layout = {
+    init: function () {
+        var _this = this;
+        _this.documentOnClick();
+    },
+    documentOnClick: function () {
+        if (window != window.parent) { // 如果是在iframe中
+            $(document).click(function (event) {
+                $(window.parent.document)
+                    .find('#page-heard ul.nav-menu > li.item') //收起父级通知栏
+                    .removeClass('open');
+            });
+        }
+    }
+}
